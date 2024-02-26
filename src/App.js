c3ad42c258d725de219fbbd52355c576a922c56b
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2024-02-26 21:24:12 +0100

    vezba router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Home, Users, About } from "./components";
+import UsersContextProvider from "./context/UserContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UsersContextProvider>
+        {/* Router nam omogucava upotrebu rutiranja u celoj aplikaciji - Wrapper */}
+        <Router>
+          <div className="navbar">
+            {/* Link komponenta za navigaciju ka razlicitim rutama. 
+          Svaki link ima svoj "to" prop koji definise putanju ka odgovarajucoj ruti */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/users">Users</Link>
+          </div>
+          {/* Unutar Routes komponente definisu se pojedinacne rute*/}
+          <Routes>
+            {/* Svaka ruta ima svoj "path" prop koji definise putanju ka kojoj ruta vodi,
+          i "element" prop koji sadrzi komponentu koja treba da se renderuje kada se ruta 
+          poklopi sa trenutnom putanjom*/}
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </UsersContextProvider>
     </div>
   );
 }
